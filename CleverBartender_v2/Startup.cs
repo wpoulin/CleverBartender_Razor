f4803154f7af1d6a15089010fbc0461ada2a93e4
William Poulin <pouw2201@usherbrooke.ca>
2018-12-04 15:30:27 -0500

    master clearnup

--- CleverBartender_v2/Startup.cs
+++ CleverBartender_v2/Startup.cs
@@ -67,38 +67,6 @@ namespace CleverBartender_v2
                     name: "default",
                     template: "{controller=Drinks}/{action=Index}/{id?}");
             });
-
-            app.UseWebSockets();
-
-            var webSocketOptions = new WebSocketOptions()
-            {
-                KeepAliveInterval = TimeSpan.FromSeconds(120),
-                ReceiveBufferSize = 4 * 1024
-            };
-            app.UseWebSockets(webSocketOptions);
-
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.Path == "/ws")
-                {
-                    if (context.WebSockets.IsWebSocketRequest)
-                    {
-                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                        //await Echo(context, webSocket);
-                    }
-                    else
-                    {
-                        context.Response.StatusCode = 400;
-                    }
-                }
-                else
-                {
-                    await next();
-                }
-
-            });
-
-
         }
     }
 }
